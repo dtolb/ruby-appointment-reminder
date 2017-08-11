78d670548333f092ab5d4e8f36d5f6ae3964f864
avbel <belchikovs@gmail.com>
2017-08-11 10:53:33 +0300

    Fixes in detection of protocol

--- host_backend.rb
+++ host_backend.rb
@@ -7,7 +7,7 @@ class HostBackend
 
  def call(env)
    db = env["database"]
-   db["HostData"].update_one({}, {host: env["SERVER_NAME"], protocol: env["SERVER_PROTOCOL"]}, {upsert: true})
+   db["HostData"].update_one({}, {host: env["SERVER_NAME"], protocol: env["HTTP_X_FORWARDED_PROTO"] || env["rack.url_scheme"]}, {upsert: true})
    @app.call(env)
  end
 end
